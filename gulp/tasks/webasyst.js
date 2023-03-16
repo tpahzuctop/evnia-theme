a4df22495e13b97f241fc872d1884700fdc0abf9
Alexey Efimov <tpahzuctop@ya.ru>
2023-03-16 13:34:02 +0300

    theme setting change

--- gulp/tasks/webasyst.js
+++ gulp/tasks/webasyst.js
@@ -1,14 +1,14 @@
 export const webasystSiteCss = () => {
-    return app.gulp.src(app.path.src.webasyst_evnia_css)
-        .pipe(app.gulp.dest(app.path.build.webasyst_evnia_css))
+    return app.gulp.src(app.path.src.webasyst_site_css)
+        .pipe(app.gulp.dest(app.path.build.webasyst_site_css))
 }
 
 export const webasystSiteJs = () => {
-    return app.gulp.src(app.path.src.webasyst_evnia_js)
-        .pipe(app.gulp.dest(app.path.build.webasyst_evnia_js))
+    return app.gulp.src(app.path.src.webasyst_site_js)
+        .pipe(app.gulp.dest(app.path.build.webasyst_site_js))
 }
 
 export const webasystSiteFonts = () => {
     return app.gulp.src(app.path.src.fonts)
-        .pipe(app.gulp.dest(app.path.build.webasyst_evnia_fonts))
+        .pipe(app.gulp.dest(app.path.build.webasyst_site_fonts))
 }
